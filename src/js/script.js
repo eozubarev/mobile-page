29f4affe40ac60130a117de399390c7746103b66
Evgeny <ahotickkk@mail.ru>
2020-07-15 16:32:04 +0300

    add src files

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,69 @@
-@@include("alert.js");
-@@include("marina.js");
+//btn animation start
 
-/* JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP START*/
+var buttons = document.getElementsByClassName('butt'),
+    forEach = Array.prototype.forEach;
 
-function testWebP(callback) {
-  var webP = new Image();
-  webP.onload = webP.onerror = function() {
-    callback(webP.height == 2);
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-testWebP(function(support) {
-  if (support == true) {
-    document.querySelector("body").classList.add("webp");
-  } else {
-    document.querySelector("body").classList.add("no-webp");
-  }
+forEach.call(buttons, function (b) {
+    b.addEventListener('click', addElement);
 });
 
-/* JS-ФУНКЦИЯ ОПРЕДЕЛЕНИЯ ПОДДЕРЖКИ WEBP END*/
+function addElement(e) {
+    var addDiv  = document.createElement('div'),
+        mValue  = Math.max(this.clientWidth, this.clientHeight),
+        rect    = this.getBoundingClientRect();
+        sDiv    = addDiv.style,
+        px      = 'px';
 
+    sDiv.width  = sDiv.height = mValue + px;
+    sDiv.left  = e.clientX - rect.left - (mValue / 2) + px;
+    sDiv.top   = e.clientY - rect.top - (mValue / 2) + px;
+
+    addDiv.classList.add('pulse');
+    this.appendChild(addDiv);
+}
+
+//btn animation end
+
+//change theme start
+
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "css/light-theme.css";
+    let darkTheme = "css/dark-theme.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    // Save(theme);
+}
+
+//change theme end
+
+function toggleTheme(theme) {
+    const btn = document.getElementById("theme-button");
+    btn.classList.remove('dark', 'light')
+    if (theme == 'dark') {
+        btn.classList.add('dark')
+    } else {
+        btn.classList.add('light')
+    }
+}
 
