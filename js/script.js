1ec2bf02288b6efd3a40f3e89be680095d32ac66
Evgeny <ahotickkk@mail.ru>
2020-07-15 14:32:22 +0300

    Upload Task Test for a BingoBoom(ATMOSPHERE)

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+//btn animation start
+
+var buttons = document.getElementsByClassName('butt'),
+    forEach = Array.prototype.forEach;
+
+forEach.call(buttons, function (b) {
+    b.addEventListener('click', addElement);
+});
+
+function addElement(e) {
+    var addDiv  = document.createElement('div'),
+        mValue  = Math.max(this.clientWidth, this.clientHeight),
+        rect    = this.getBoundingClientRect();
+        sDiv    = addDiv.style,
+        px      = 'px';
+
+    sDiv.width  = sDiv.height = mValue + px;
+    sDiv.left  = e.clientX - rect.left - (mValue / 2) + px;
+    sDiv.top   = e.clientY - rect.top - (mValue / 2) + px;
+
+    addDiv.classList.add('pulse');
+    this.appendChild(addDiv);
+}
+
+// btn animation end
+
+// change theme start
+
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "css/light-theme.css";
+    let darkTheme = "css/dark-theme.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    // Save(theme);
+}
+
+// change theme end
